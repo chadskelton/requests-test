98dfb6d3243b9b2a73bbe4a7e5957c887d803b45
Chad Skelton <cskeltonnews@gmail.com>
2019-06-18 12:18:37 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,2 +1,6 @@
 import requests
+import scraperwiki
+
+scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+
 r = requests.post('https://bweb.kwantlen.ca/pls/prodss/bwysched.p_search_fields', data = {'term_code':'201920', 'wsea_code':'ACAD'})
