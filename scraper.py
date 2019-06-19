3220f43ad463e95a5542197e0ebabecd2d3e2930
Chad Skelton <cskeltonnews@gmail.com>
2019-06-19 09:30:32 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,6 +3,6 @@ import scraperwiki
 
 scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 
-r = requests.post('https://bweb.kwantlen.ca/pls/prodss/bwysched.p_search_fields', data = {'term_code':'201920', 'wsea_code':'ACAD'})
+r = requests.post('https://bweb.kwantlen.ca/pls/prodss/bwysched.p_search_fields')
 
 print r
