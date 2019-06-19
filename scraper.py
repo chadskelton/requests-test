31625b23ed3599a63a64ea64c9610dbbcb87d31b
Chad Skelton <cskeltonnews@gmail.com>
2019-06-19 09:30:57 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,6 +3,6 @@ import scraperwiki
 
 scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 
-r = requests.post('https://bweb.kwantlen.ca/pls/prodss/bwysched.p_search_fields')
+r = requests.post('http://vancouversun.com/')
 
 print r
